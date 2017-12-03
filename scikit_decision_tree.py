aa5416643404f1f6e72b069f888c167a0f3ab5ab
sandzu <a.schaefer1995@gmail.com>
2017-12-03 11:35:46 -0800

    basic off-shelf support vector machine

--- scikit_decision_tree.py
+++ scikit_decision_tree.py
@@ -1,9 +1,9 @@
-import matplotlibpyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 def gini(p):
     return p*(1-p) + (1-p)*(1-(1-p)) # ...why not just use 2*P*(1-p)
 def entropy(p):
-    reutnr - p*np.log2(p) - (1-p)*np.log2((1-p))
+    return - p*np.log2(p) - (1-p)*np.log2((1-p))
 def error(p):
     return 1 - np.max([p,1-p])
 
@@ -13,4 +13,28 @@ sc_ent = [e*0.5 if e else None for e in ent]
 err = [error(i) for i in x]
 fig = plt.figure()
 ax = plt.subplot(111)
+for i, lab,ls,c, in zip([ent,sc_ent,gini(x), err,],
+                        ['Entropy','Entropy (scaled)', 'Gini Impurity', 'Misclassification Error'],
+                        ['-', '-', '--', '-.'],
+                        ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls,lw=2, color=c)
 
+ax.legend(loc='upper center', bbox_to_anchor=(.5,1.15), ncol=3, fancybox=True, shadow=False)
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('impurity index')
+plt.show()
+
+from sklearn import datasets
+iris = datasets.load_iris()
+X = iris.data[:,[2,3]]
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state = 0)
+
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3,random_state=0)
+tree.fit(X_train, y_train)
---
+++ scikit_models_svm.py
@@ -0,0 +1,203 @@
+"""scikit basics with  Sebastian Raschka - code modified by andzu schaefer"""
+#todo: create standalone files, depreciation prep
+'''here we will classify some flowers with help from the sklearn.linear_model's Perceptron'''
+
+'''initialization and data prep'''
+from sklearn import datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+# if we executed np.unique(y)...
+np.unique(y)  # should see that the iris flow class names are already stored as integers.
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()  # initialize new standardscaler object
+sc.fit(X_train)  # estimate mean, std of X_train
+X_train_std = sc.transform(X_train)  # standardize training data
+X_test_std = sc.transform(X_test)  # standardize test data
+
+'''train the perceptron'''
+# most algs in scikit-learn already support multiclass classification via One-vs-Rest (OVR)
+from sklearn.linear_model import Perceptron
+
+ptron = Perceptron(n_iter=40, eta0=0.01, random_state=0)
+# eta is learning rate
+ptron.fit(X_train_std, y_train)
+y_pred = ptron.predict(X_test_std)
+
+'''check the performance of the perceptron model'''
+from sklearn.metrics import accuracy_score
+print('accuracy : %.2f' % accuracy_score(y_test, y_pred))
+
+
+# plot some decision regions
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1,
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1,
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max,
+                                     resolution))  # arange creates arrays with numbers from first param to second param at intervals of third param
+    # meshgrid functions as explained here: http://louistiao.me/posts/numpy-mgrid-vs-meshgrid/
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)  # np.ravel flattens arrays,
+    # .T : Same as self.transpose(), except that self is returned if self.ndim < 2.
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.04, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot all samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidths=1, marker='o', s=55, label='test set')
+
+
+X_combined_std = np.vstack((X_train_std,
+                            X_test_std))
+y_combined = np.hstack((y_train, y_test)) # Take a sequence of arrays and stack them horizontally to make a single array
+plot_decision_regions(X=X_combined_std,
+                      y=y_combined,
+                      classifier=ptron,
+                      test_idx=range(105, 150))
+# https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.scatter documentation
+plt.xlabel('petal length[standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+'''logistic regression'''
+# scikit learn implements a highly optimized version of logistic
+#  regression that also supports multiclass settings off the shelf
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+# C is regularization strength, see more below
+lr.fit(X_train_std, y_train)
+
+'''explore capabilities of the logistic regression model'''
+plot_decision_regions(X=X_combined_std,
+                      y=y_combined,
+                      classifier=lr,  # we change which classifier we use
+                      test_idx=range(105, 150))
+# https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.scatter documentation
+plt.xlabel('petal length[standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+# we can predict class membership probabilty (confidence) using the predict_proba method
+prob = lr.predict_proba(X_test_std[0, :])
+print(prob[0][2])
+
+'''tackling overfitting with regularization '''
+# train a series of models and compare weights/regularization strength rations
+weights, params = [], []
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10 ** float(c), random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10 ** float(c))
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],
+         label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',
+         label='petal width')
+plt.ylabel('weight coeff')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
+
+''''Support Vector Machine '''
+from sklearn.svm import SVC
+
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,
+                      y_combined,
+                      classifier=svm,
+                      test_idx=range(105,150))
+
+plt.xlabel('petal length[standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+y_pred = svm.predict(X_test_std)
+print('accuracy : %.2f' % accuracy_score(y_test, y_pred))
+
+''''''
+'''solving nonlinear problems using a kernel SVM'''
+np.random.seed(0)
+X_xor = np.random.randn(200,2) #returns a 200 x 2 array with samples from the std normal distribution
+y_xor = np.logical_xor(X_xor[:,0] > 0, X_xor[:,1]>0)
+y_xor = np.where(y_xor,1,-1) #(condition[, x, y])¶ Return elements, either from x or y,
+#  depending on condition.
+
+#plot the xor data
+plt.scatter(X_xor[y_xor==1,0], X_xor[y_xor==1,1],
+            c='b', marker='x', label='1')
+
+plt.scatter(X_xor[y_xor==-1,0], X_xor[y_xor==-1,1],
+            c='r', marker='s', label='-1')
+plt.ylim=(-3,0)
+plt.legend()
+plt.show()
+
+#train the SVM
+svm = SVC(kernel='rbf', C=10.0, gamma = 0.10, random_state=0) #notice kernel = rbf
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor,
+                      y_xor,
+                      classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
+
+#gamma is the parameter that controls the 'cut-off' of the gaussian sphere
+svm = SVC(kernel='rbf', C=1.0, gamma = 0.20, random_state=0)
+svm.fit(X_train_std,y_train)
+plot_decision_regions(X_combined_std,
+                      y_combined,
+                      classifier=svm,
+                      test_idx=range(105,150))
+
+plt.xlabel('petal length[standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+#gamma is the parameter that controls the 'cut-off' of the gaussian sphere
+svm = SVC(kernel='rbf', C=1.0, gamma = 100, random_state=0)
+svm.fit(X_train_std,y_train)
+plot_decision_regions(X_combined_std,
+                      y_combined,
+                      classifier=svm,
+                      test_idx=range(105,150))
+
+plt.xlabel('petal length[standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
