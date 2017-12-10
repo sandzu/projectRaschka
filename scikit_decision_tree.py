05016a353562ed10626b60220641cdaca454889d
sandzu <a.schaefer1995@gmail.com>
2017-12-09 23:50:58 -0800

    iris classification using a decision tree

--- scikit_decision_tree.py
+++ scikit_decision_tree.py
@@ -7,6 +7,9 @@ def entropy(p):
 def error(p):
     return 1 - np.max([p,1-p])
 
+
+
+
 x = np.arange(0.0, 1.0,.01)
 ent = [entropy(p) if p != 0 else None for p in x]
 sc_ent = [e*0.5 if e else None for e in ent]
@@ -38,3 +41,41 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_st
 from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier(criterion='entropy', max_depth=3,random_state=0)
 tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.vstack((y_train, y_test))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    #setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1,
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1,
+    xx1, xx2 = np.meshgrid(np.arange(x1_min,x1_max, resolution),
+                           np.arange(x2_min,x2_max, resolution)) #arange creates arrays with numbers from first param to second param at intervals of third param
+    #meshgrid functions as explained here: http://louistiao.me/posts/numpy-mgrid-vs-meshgrid/
+    Z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T) #np.ravel flattens arrays,
+    #.T : Same as self.transpose(), except that self is returned if self.ndim < 2.
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1,xx2, Z, alpha=0.04,cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    #plot all samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==cl,0], y=X[y==cl,1], alpha=0.8, c=cmap(idx), marker=markers[idx],label=cl)
+
+    #highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx,:], y[test_idx]
+        plt.scatter(X_test[:,0], X_test[:,1], c='', alpha=1.0, linewidths=1,marker='o',s=55, label='test set')
+
+plot_decision_regions(X_combined, y_combined, classifier=tree, test_idx=range(105,150))
+
+plt.xlabel('petal length[cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
